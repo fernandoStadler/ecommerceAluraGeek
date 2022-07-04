95246c6012d908d3847f837d11fd5e19cd3f6b9e
Fernando Stadler <fernandostadler2@gmail.com>
2022-07-04 09:19:27 -0300

    criação pagina principal

---
+++ javascript/api/data.js
@@ -0,0 +1,57 @@
+  let products = [
+    {
+        "id":1,
+        "tumbnail":"",
+        "category":"",
+        "product_name":"",
+        "product_price":"",
+        "short_description":"",
+        "active":"",
+    },
+    {
+        "id":2,
+        "tumbnail":"",
+        "category":"",
+        "product_name":"",
+        "product_price":"",
+        "short_description":"",
+        "active":"",
+    },
+    {
+        "id":3,
+        "tumbnail":"",
+        "category":"",
+        "product_name":"",
+        "product_price":"",
+        "short_description":"",
+        "active":"",
+    },
+    {
+        "id":4,
+        "tumbnail":"",
+        "category":"",
+        "product_name":"",
+        "product_price":"",
+        "short_description":"",
+        "active":"",
+    },
+]
+
+ let users = [
+    {
+        "id": "",
+        "user_name": "",
+        "email": "",
+        "password": "",
+        "admin_state": 0,
+        "active": 1,
+    },
+    {
+        "id": "",
+        "user_name": "",
+        "email": "",
+        "password": "",
+        "admin_state": 1,
+        "active": 1,
+    }
+];
---
+++ javascript/app/index.js
@@ -0,0 +1,5 @@
+
+for (var i = 0; i < products.length; i++) {
+
+    console.table(products[i]);  
+}
